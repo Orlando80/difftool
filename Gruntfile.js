9adbf62cf5e50690cfd44a6ce5654e4d367e7422
orlando <operri@opentable.com>
2015-05-11 17:11:38 +0100

    added bump config

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,8 +4,28 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       files: ['index.js','Gruntfile','test/**/*.js']
+    },
+ bump: {
+    options: {
+      files: ['package.json'],
+      updateConfigs: [],
+      commit: true,
+      commitMessage: 'Release v%VERSION%',
+      commitFiles: ['package.json'],
+      createTag: true,
+      tagName: 'v%VERSION%',
+      tagMessage: 'Version %VERSION%',
+      push: true,
+      pushTo: 'origin',
+      gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+      globalReplace: false,
+      prereleaseName: false,
+      regExp: false
     }
+  }
   });
+  grunt.loadNpmTasks('grunt-bump');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.registerTask('lint',['jshint']);
+  grunt.registerTask('deploy',['bump']);
 };
